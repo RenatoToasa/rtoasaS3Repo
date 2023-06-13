4162d7442ac703f186137d661e8da9302c4969be
master <master@SMLAB310PC01_23>
2023-06-12 19:21:13 -0500

    Agregar archivos de proyecto.

---
+++ rtoasaS3/Page1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace rtoasaS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string datoUno = txtDatoUno.Text;
+            int datoDos = Convert.ToInt32(txtDatoDos.Text);
+
+            Navigation.PushAsync(new Page2(datoUno, datoDos));
+        }
+    }
+}
---
+++ rtoasaS3/Page2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace rtoasaS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2(string datoUno, int datoDos)
+        {
+            InitializeComponent();
+            lblDatoUno.Text ="DATO UNO "+ datoUno;
+            lblDatoDos.Text= "DATO DOS " + datoDos.ToString();
+        }
+
+        private void btnAbrirP_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+    }
+}
